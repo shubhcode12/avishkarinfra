d81e145b4b2365e641cd1a0a78b3d3be0c3649ab
Sourabh Davari <sourabhdavari@gmail.com>
2024-09-22 15:51:55 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://images.unsplash.com', ],
+    loader: 'akamai',
+    path: '',
+    unoptimized : true
+  },
 };
 
 export default nextConfig;
