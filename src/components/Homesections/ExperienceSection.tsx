5d7d86ea16bd4cff1f3c39236d5be463806b7fb4
shubhcode <shubhcode.dev@gmail.com>
2024-09-28 14:30:29 +0530

    rename section2 to exp section

---
+++ src/components/Homesections/ExperienceSection.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import ExperienceCounts from './ExperienceCounts'
+
+const ExperienceSection = () => {
+  return (
+    <section className="w-full pt-24 justify-center px-4 lg:px-10 ">
+    <div className="flex justify-center w-full">
+      <div className="px-8 w-full border-b pb-12">
+        <div className="flex justify-center w-full gap-8">
+          <div className="flex flex-col w-full gap-8">
+            <div className="text-6xl font-semibold text-secondary uppercase leading-[4rem] text-transparent bg-gradient-to-r from-secondary to-gray-300 inline-block bg-clip-text">
+              We've found luxury homes for clients for a decade.
+            </div>
+            <div className="text-lg text-md">
+              We take great pride in ensuring the satisfaction of our
+              customers, which is why we guarantee that the products we sell
+              will bring happiness to each and every customer. Our genuine
+              care for customer satisfaction is what sets us apart.
+            </div>
+          </div>
+          <div className="w-auto lg:w-2/3"></div>
+        </div>
+      </div>
+    </div>
+    <ExperienceCounts />
+  </section>
+  )
+}
+
+export default ExperienceSection
