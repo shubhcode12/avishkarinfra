313b6f0ede05eec50ad169b491da2a2e03722ebd
shubhcode <shubhcode.dev@gmail.com>
2024-09-28 13:21:28 +0530

    fix responsive

---
+++ src/components/Homesections/KeyPointsSection.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { CardHoverEffectDemo } from "../common/Goal";
+import { Button } from "../ui/button";
+import { common } from "../../../public/assets/icon";
+import Image from "next/image";
+const KeyPointsSection = () => {
+  return (
+    <div className="mx-4">
+      <section className="w-full flex py-24 justify-center px-4 lg:px-8 bg-secondary rounded-3xl hero-banner">
+        <div className="flex justify-center w-full">
+          <div className="px-4 w-full ">
+            <div className="flex flex-col gap-8 lg:flex-row justify-center lg:justify-between  items-end w-full">
+              <div className="text-white font-semibold text-3xl lg:text-5xl max-w-3xl">
+                Where Innovation, Sustainability, and Community Meet
+              </div>
+              <div className="flex justify-start w-full lg:w-auto">
+                <Button className="flex justify-center w-auto gap-2">
+                  <span>View All Properties</span>
+                  <span>
+                    <Image src={common.arrowCrossRight} alt="" />
+                  </span>
+                </Button>
+              </div>
+            </div>
+            <div className="mt-12 lg:mt-16">
+              <CardHoverEffectDemo />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default KeyPointsSection;
