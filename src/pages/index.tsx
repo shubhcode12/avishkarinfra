468b0dacaed3e3a7ce75ae5d9e5f7e55b1bde1c9
shubhcode <shubhcode.dev@gmail.com>
2024-09-28 13:22:44 +0530

    code clean & remove unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,61 +1,31 @@
-import Image from "next/image";
-import localFont from "next/font/local";
-import { ParallaxScroll } from "@/components/ui/parallax-scroll";
-import { ButtonsCard } from "@/components/ui/tailwindcss-buttons";
-import { Button } from "@/components/ui/button";
 import Navbar from "@/components/common/Navbar";
-import styles from "./page.module.css";
-import Navbar2 from "@/components/common/Header";
-import { Homescreen } from "../../public/assets/images";
-import { common } from "../../public/assets/icon";
-import { useState } from "react";
-import CardDemo from "@/components/blocks/cards-demo-2";
-import { BentoGrid, BentoGridItem } from "@/components/ui/bento-grid2";
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion";
-import {
-  BentoGrid as BentoGrid2,
-  BentoGridItem as BentoGridItem2,
-} from "@/components/ui/bento-grid";
+import { AppleCardsCarouselDemo } from "@/components/common/Reviews";
 import {
   IconArrowWaveRightUp,
   IconBoxAlignRightFilled,
   IconBoxAlignTopLeft,
   IconClipboardCopy,
-  IconClockFilled,
   IconFileBroken,
-  IconLocationFilled,
-  IconMapPinFilled,
-  IconPhoneFilled,
-  IconSend,
   IconSignature,
-  IconTableColumn,
+  IconTableColumn
 } from "@tabler/icons-react";
-import { AppleCardsCarouselDemo } from "@/components/common/Reviews";
-
+import { useState } from "react";
 
 import Footer from "@/components/common/Footer";
-import { CardHoverEffectDemo } from "@/components/common/Goal";
 import HeroWithOceanVideo from "@/components/common/HeroVideo";
+import ContactSection from "@/components/Homesections/ContactSection";
+import Homefinal from "@/components/Homesections/Homefinal";
+import KeyPointsSection from "@/components/Homesections/KeyPointsSection";
 import Section1 from "@/components/Homesections/Section1";
 import Section2 from "@/components/Homesections/Section2";
 import Section3 from "@/components/Homesections/Section3";
-import KeyPointsSection from "@/components/Homesections/KeyPointsSection";
 import Section5 from "@/components/Homesections/Section5";
 import Section6 from "@/components/Homesections/Section6";
 import Section7 from "@/components/Homesections/Section7";
 import Section8 from "@/components/Homesections/Section8";
-import ContactSection from "@/components/Homesections/ContactSection";
-import Homefinal from "@/components/Homesections/Homefinal";
 const Skeleton = () => (
   <div className="flex flex-1 w-full h-full min-h-[6rem] rounded-xl bg-gradient-to-br from-neutral-200 dark:from-neutral-900 dark:to-neutral-800 to-neutral-100"></div>
 );
-
-
 
 const items = [
   {
@@ -103,7 +73,6 @@ const items = [
   },
 ];
 
-
 export default function Home() {
   const [propertyType, setpropertyType] = useState("New to market");
   return (
@@ -111,27 +80,22 @@ export default function Home() {
       <div className="fixed w-full z-10">
         <Navbar />
       </div>
-      <HeroWithOceanVideo/>
-      <Section1/>
-      <Section2/>
-      <Section3/>
-      <KeyPointsSection/>
-      <Section5/>
-      <Section6/>
-      <Section7/>
+      <HeroWithOceanVideo />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <KeyPointsSection />
+      <Section5 />
+      <Section6 />
+      <Section7 />
       <section className="bg-[url('../../public/assets/images/home/reviewBg.png')] lg:bg-cover bg-contain bg-no-repeat">
         <AppleCardsCarouselDemo />
       </section>
-      <Section8/>
-      <ContactSection/>
-      <Homefinal/>
-      
-      
-    
-      
-      
-      
-   <Footer/>
+      <Section8 />
+      <ContactSection />
+      <Homefinal />
+
+      <Footer />
     </div>
   );
 }
