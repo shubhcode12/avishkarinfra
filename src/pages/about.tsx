f5bdddacc089396f4fef7fc6e66446701b6247d0
shubhcode <shubhcode.dev@gmail.com>
2024-10-02 02:15:39 +0530

    TODO : polish about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -8,15 +8,17 @@ import { AppleCardsCarouselDemo } from "@/components/common/Team";
 import { StickyScrollRevealDemo } from "@/components/common/Sticky";
 import { useRouter } from "next/router";
 import Footer from "@/components/common/Footer";
+import TextRevealByWord from "@/components/ui/TextReveal";
 const About = () => {
   const router = useRouter();
   return (
     <div>
       <Navbar />
-      <section className="w-full  flex pt-32 pb-24 justify-center px-4 lg:px-0">
+      <section className="w-full flex py-14 pt-28 lg:pt-40 justify-center px-4 lg:px-10 ">
         <div className="flex px-8 justify-center w-full">
           <div className=" w-full ">
-            <div className="flex flex-col gap-8">
+            {/* <TextRevealByWord text="Welcome to Avishkar Infra! Best builders and developers in Kolhapur"/> */}
+            {/* <div className="flex flex-col gap-8">
               <div className="flex">
                 <div className="w-full flex flex-col justify-evenly">
                   <div className="text-secondary font-semibold text-6xl">
@@ -31,8 +33,7 @@ const About = () => {
                   <div className="flex justify-start items-end">
                     <Button
                       onClick={() => router.push("/contactus")}
-                      className="flex justify-center gap-2"
-                    >
+                      className="flex justify-center gap-2">
                       <span>Contact us</span>
                       <span>
                         <Image src={common.arrowCrossRight} alt="" />
@@ -53,17 +54,17 @@ const About = () => {
                   />
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </div>
       </section>
-      <section className="w-full bg-[#666666] bg-opacity-20 flex pt-32 pb-24 justify-center px-4 lg:px-0">
+      <section className="w-full bg-[#f7f7f7] flex pt-32 pb-24 justify-center px-4 lg:px-0">
         <div className="flex px-8 justify-center w-full">
           <div className=" w-full ">
             <div className="flex flex-col gap-8">
-                  <div className="text-4xl text-secondary font-semibold">
-                    About Avishkar Infra
-                  </div>
+              <div className="text-4xl text-secondary font-semibold">
+                About Avishkar Infra
+              </div>
               <div className="flex flex-col-reverse lg:flex-row gap-4 justify-center items-center">
                 <div className="w-full">
                   <div>
@@ -97,9 +98,12 @@ const About = () => {
                 </div>
                 <div className="w-full">
                   <div className="text-4xl text-secondary font-semibold">
-                  Vision & Mission
+                    Vision & Mission
                   </div>
-                  To set new benchmarks in the real estate industry by continuously pushing boundaries, delivering innovative, premium-quality projects, and shaping the future skyline of Kolhapur.
+                  To set new benchmarks in the real estate industry by
+                  continuously pushing boundaries, delivering innovative,
+                  premium-quality projects, and shaping the future skyline of
+                  Kolhapur.
                 </div>
               </div>
             </div>
@@ -138,8 +142,7 @@ const About = () => {
           </div>
         </div>
       </section>
-      
-      
+
       <section className="w-full bg-[#666666] bg-opacity-20 flex flex-col justify-center px-4 pt-24 lg:px-0">
         <div className="flex justify-center w-full">
           <div className="max-w-6xl w-full">
@@ -169,22 +172,43 @@ const About = () => {
                 </Button>
               </div> */}
               <div className="text-start max-w-3xl text-primary font-semibold text-4xl">
-              Welcome to Avishkar Infra! Best builders and developers in Kolhapur
+                Welcome to Avishkar Infra! Best builders and developers in
+                Kolhapur
               </div>
             </div>
-        <div className="text-secondary text-xl my-12 font-thin">
-        Avishkar Infra is a distinguished name among real estate builders in Kolhapur, and it is known for spearheading numerous prestigious and iconic projects in the city. Our vision to exceed and our passion to excel drive us to push boundaries and set new standards in the industry. As the leading builders in Kolhapur, we leverage our extensive experience and deep knowledge to develop premium, high-quality real estate projects.
-
-Our commitment to quality construction is unwavering. We prioritize using top-tier materials and innovative techniques to ensure every project we undertake meets the highest standards of excellence. This dedication has not only enhanced our reputation but also established us as one of the most trusted land developers in Kolhapur.
-
-At Avishkar Infra, we believe in creating spaces that blend convenience with luxury. Our projects are thoughtfully designed to provide amenities that cater to modern lifestyles, ensuring our customers enjoy a comfortable and fulfilling living experience. Whether it&apos;s a premium bungalow or a large-scale development, we focus on every detail to deliver homes that our customers can be proud of.
-
-Our services encompass Design and Build, Construction Management, and the development of Premium Bungalows. Each service is tailored to meet the unique needs of our clients, ensuring a seamless and satisfying experience from concept to completion. Building on a foundation of quality and trust, Avishkar Infra continues to shape the skyline of Kolhapur, transforming visions into reality and setting benchmarks in real estate development. With every project, we strive to exceed expectations and contribute to the city's growth, reinforcing our position as a leader in the industry.
-        </div>
+            <div className="text-secondary text-xl my-12 font-thin">
+              Avishkar Infra is a distinguished name among real estate builders
+              in Kolhapur, and it is known for spearheading numerous prestigious
+              and iconic projects in the city. Our vision to exceed and our
+              passion to excel drive us to push boundaries and set new standards
+              in the industry. As the leading builders in Kolhapur, we leverage
+              our extensive experience and deep knowledge to develop premium,
+              high-quality real estate projects. Our commitment to quality
+              construction is unwavering. We prioritize using top-tier materials
+              and innovative techniques to ensure every project we undertake
+              meets the highest standards of excellence. This dedication has not
+              only enhanced our reputation but also established us as one of the
+              most trusted land developers in Kolhapur. At Avishkar Infra, we
+              believe in creating spaces that blend convenience with luxury. Our
+              projects are thoughtfully designed to provide amenities that cater
+              to modern lifestyles, ensuring our customers enjoy a comfortable
+              and fulfilling living experience. Whether it&apos;s a premium
+              bungalow or a large-scale development, we focus on every detail to
+              deliver homes that our customers can be proud of. Our services
+              encompass Design and Build, Construction Management, and the
+              development of Premium Bungalows. Each service is tailored to meet
+              the unique needs of our clients, ensuring a seamless and
+              satisfying experience from concept to completion. Building on a
+              foundation of quality and trust, Avishkar Infra continues to shape
+              the skyline of Kolhapur, transforming visions into reality and
+              setting benchmarks in real estate development. With every project,
+              we strive to exceed expectations and contribute to the city's
+              growth, reinforcing our position as a leader in the industry.
+            </div>
           </div>
         </div>
       </section>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
