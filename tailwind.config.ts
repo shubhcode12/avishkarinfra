fdee208c7c83059e86bd573e3ef5c6fddb06917b
shubhcode <shubhcode.dev@gmail.com>
2024-10-03 21:10:57 +0530

    build fix tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,7 @@ const config = {
   prefix: "",
   
   theme: {
-  	container: {
-  		center: 'true',
+  	container: {  		
   		padding: '2rem',
   		screens: {
   			'2xl': '1400px'
@@ -92,29 +91,11 @@ const config = {
   				to: {
   					height: '0'
   				}
-  			},
-  			'accordion-down': {
-  				from: {
-  					height: '0'
-  				},
-  				to: {
-  					height: 'var(--radix-accordion-content-height)'
-  				}
-  			},
-  			'accordion-up': {
-  				from: {
-  					height: 'var(--radix-accordion-content-height)'
-  				},
-  				to: {
-  					height: '0'
-  				}
-  			}
+  			},  			
   		},
   		animation: {
   			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
-  			'accordion-down': 'accordion-down 0.2s ease-out',
-  			'accordion-up': 'accordion-up 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out',  			
 			'marquee': 'marquee 25s linear infinite',
   		}
   	}
