1c337f2c2d6d9b7dce9cc7c197e2148beb8bdcc0
shubhcode <shubhcode.dev@gmail.com>
2024-10-04 10:58:48 +0530

    implement lenis scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { ReactLenis } from "@/lib/lenis";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <ReactLenis root>
+      <Component {...pageProps} />
+    </ReactLenis>
+  );
 }
